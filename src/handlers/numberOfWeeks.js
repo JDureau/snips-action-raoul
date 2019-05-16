f52c8fe40c5d9fce7d7a4f497d30ff7f1bb82e6b
Joseph Dureau <joseph.dureau@snips.ai>
2019-05-16 22:55:49 +0200

    test

--- src/handlers/numberOfWeeks.js
+++ src/handlers/numberOfWeeks.js
@@ -6,7 +6,7 @@ module.exports = async function (msg, flow) {
     // Suppose we have a pokemon id slot
     // If there are multiple, we take the only that is supposed to be the 'most valid'.
     // We discard slots with a confidence value too low.
-    console.log("IN")
+    console.log(JSON.stringify(msg))
     //const pokemonSlot = message.getSlotsByName(msg, 'pokemon_id', { onlyMostConfident: true, threshold: 0.5 })
 
     // We need this slot, so if the slot had a low confidence or was not mark as required,
@@ -19,12 +19,13 @@ module.exports = async function (msg, flow) {
     //const pokemon = await api.getPokemon(pokemonSlot.value.value)
 
     // End the dialog session.
-    // flow.end()
+    flow.end()
 
     // Return the TTS speech.
     const i18n = i18nFactory.get()
     //const pokemonName = pokemon.name
-    return i18n('raoul.info', {
-       name: "Arsène"
-    })
+    return "YO"
+    // return i18n('raoul.info', {
+    //    name: "Arsène"
+    // })
 }
