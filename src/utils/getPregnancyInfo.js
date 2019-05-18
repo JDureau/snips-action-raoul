7d634456746a237ffffeb904c4efef96c0ad5f89
Joseph Dureau <joseph.dureau@snips.ai>
2019-05-18 21:38:55 +0200

    bugfic

--- src/utils/getPregnancyInfo.js
+++ src/utils/getPregnancyInfo.js
@@ -7,7 +7,7 @@ module.exports = {
       const conception = new Date('December 27, 2018');
       const now = new Date(Date.now());
       const diff = now.getDate() - conception.getDate();
-      const diffWeeks = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 7));
+      const diffWeeks = Math.ceil(diff / (1000 * 60 * 60 * 24 * 7));
       return pregnancyData[diffWeeks - 1];
   }
 }
