cd3a469a5bd46249bf58b060579a2b96f3c41f0b
Joseph Dureau <joseph.dureau@snips.ai>
2019-05-18 21:27:48 +0200

    towards first functional intent

--- src/handlers/numberOfWeeks.js
+++ src/handlers/numberOfWeeks.js
@@ -6,26 +6,15 @@ module.exports = async function (msg, flow) {
     // Suppose we have a pokemon id slot
     // If there are multiple, we take the only that is supposed to be the 'most valid'.
     // We discard slots with a confidence value too low.
-    console.log(JSON.stringify(msg))
-    //const pokemonSlot = message.getSlotsByName(msg, 'pokemon_id', { onlyMostConfident: true, threshold: 0.5 })
 
-    // We need this slot, so if the slot had a low confidence or was not mark as required,
-    // we throw an error.
-    //if(!pokemonSlot) {
-    //    throw new Error('intentNotRecognized')
-    //}
+    const weekInfo = getPregnancyInfo.getWeekInfo()
 
-    // Get the Pokemon data
-    //const pokemon = await api.getPokemon(pokemonSlot.value.value)
-
-    // End the dialog session.
-    flow.end()
 
     // Return the TTS speech.
     const i18n = i18nFactory.get()
     //const pokemonName = pokemon.name
-    return "YO"
+    return weekInfo.weeks
     // return i18n('raoul.info', {
-    //    name: "Arsène"
+    //    semaines: "Arsène"
     // })
 }
---
+++ src/utils/getPregnancyInfo.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+module.exports = {
+  getWeekInfo: () => {
+      const pregnancyData = fs.readFileSync(path.resolve(__dirname + `/../../assets/grossesse.json`), 'utf-8')
+      const conception = new Date('December 27, 2018');
+      const now = new Date(Date.now());
+      const diff = now.getDate() - conception.getDate();
+      const diffWeeks = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 7));
+      return pregnancyData[diffWeeks - 1];
+  }
+}
