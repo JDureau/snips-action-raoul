699a603cb050a8e76255c07fd149d8b562890af5
Joseph Dureau <joseph.dureau@snips.ai>
2019-05-19 00:22:16 +0200

    test

--- src/handlers/askAgeAmenorrhea.js
+++ src/handlers/askAgeAmenorrhea.js
@@ -1,15 +1,16 @@
 const api = require('../api')
 const { i18nFactory } = require('../factories')
 const { message, getPregnancyInfo } = require('../utils')
-// const handlers = require('../handlers')
+const { askAge, askWhichWeek, askAgeAmenorrhea, askWhichWeekAmenorrhea } = require('./')
 
 module.exports = async function (msg, flow) {
     const weekInfo = getPregnancyInfo.getWeekInfo()
     const i18n = i18nFactory.get()
-    // flow.continue('Joseph:askAge', handlers.askAge)
-    // flow.continue('Joseph:askWhichWeek', handlers.askWhichWeek)
-    // flow.continue('Joseph:askAgeAmenorrhea', handlers.askAgeAmenorrhea)
-    // flow.continue('Joseph:askWhichWeekAmenorrhea', handlers.askWhichWeekAmenorrhea)
+    flow.continue('Joseph:askAge', handlers.askAge)
+    flow.continue('Joseph:askWhichWeek', handlers.askWhichWeek)
+    flow.continue('Joseph:askAgeAmenorrhea', handlers.askAgeAmenorrhea)
+    flow.continue('Joseph:askWhichWeekAmenorrhea', handlers.askWhichWeekAmenorrhea)
+
 
     return i18n('raoul.age', {
        week: weekInfo.week + 2
