8f5674f9d73c37c64be9628f65dc3f8eabaf863f
Joseph Dureau <joseph.dureau@snips.ai>
2019-05-19 00:13:04 +0200

    test

--- src/handlers/askAge.js
+++ src/handlers/askAge.js
@@ -1,16 +1,18 @@
 const api = require('../api')
 const { i18nFactory } = require('../factories')
 const { message, getPregnancyInfo } = require('../utils')
-const handlers = require('../handlers')
+// const handlers = require('../handlers')
 
 module.exports = async function (msg, flow) {
     const weekInfo = getPregnancyInfo.getWeekInfo()
     const i18n = i18nFactory.get()
 
-    flow.continue('Joseph:askAge', handlers.askAge)
-    flow.continue('Joseph:askWhichWeek', handlers.askWhichWeek)
-    flow.continue('Joseph:askAgeAmenorrhea', handlers.askAgeAmenorrhea)
-    flow.continue('Joseph:askWhichWeekAmenorrhea', handlers.askWhichWeekAmenorrhea)
+    flow.continue('Joseph:askAge', (message, flow) =>
+      return "test"
+    )
+    // flow.continue('Joseph:askWhichWeek', handlers.askWhichWeek)
+    // flow.continue('Joseph:askAgeAmenorrhea', handlers.askAgeAmenorrhea)
+    // flow.continue('Joseph:askWhichWeekAmenorrhea', handlers.askWhichWeekAmenorrhea)
 
     // dialog.publish('continue_session')
     return i18n('raoul.age', {
